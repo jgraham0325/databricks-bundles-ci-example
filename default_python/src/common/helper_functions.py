f42b4569f5885c6dd842459b6b2db3c4fb6cb738
James Graham <james.graham@databricks.com>
2024-06-10 11:09:05 +0100

    cleanup

--- default_python/src/common/helper_functions.py
+++ default_python/src/common/helper_functions.py
@@ -4,11 +4,6 @@ class HelperFunctions:
 
     @staticmethod
     def get_schema_json_path(table_name: str, layer: str) -> str:
-
-        if not table_name:
-            raise ValueError("The 'table_name' argument is required but was not provided.")
-        if not layer:
-            raise ValueError("The 'layer' argument is required but was not provided.")
         
         # Get the parent directory of this helper_functions.py file. Works locally and in Databricks.
         parent_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
